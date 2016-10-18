3ad4e745926fe7fcdcc89e5a049a04c369ba2ee6
Philo van Kemenade <philovankemenade@gmail.com>
2016-10-18 11:53:39 +0200

    add .gitignore

--- cache/compiled/files/50547e937ce58e40288cb4e11fef1ff5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/phivk/Documents/prog/_2016/sng/senXskutocnost/senxskutocnost.sng.sk/user/config/streams.yaml',
-    'modified' => 1476708507,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/9ac36017d4a64f6303b589dba66ecb9c.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/phivk/Documents/prog/_2016/sng/try/hello-grav-mediator/user/data/notifications/phivk.yaml',
-    'modified' => 1476708546,
-    'data' => [
-        0 => '10'
-    ]
-];
