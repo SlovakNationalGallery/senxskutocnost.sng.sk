54d42cfe3cbe49d909a1aa672bb453edbb27184a
Philo van Kemenade <philovankemenade@gmail.com>
2017-02-16 18:01:58 +0100

    fix smooth scroll broken on anchor tag
    resolved conflict of set hash and smooth scroll in separate onclick handlers by unifying (hacky) into smooth-scroll.js script

--- user/themes/longread/js/anchors.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    $('.anchorjs-link').click(function(event) {
-        var id = $(this).parent().attr('id');
-        location.hash = id;
-    });
-});
--- user/themes/longread/js/smooth-scroll.js
+++ user/themes/longread/js/smooth-scroll.js
@@ -1,4 +1,5 @@
 // enable smooth scrolling on internal hash anchor links    
+// also enable set hash behaviour on section titles
 document.addEventListener("DOMContentLoaded", function(event) {
 	$('a[href*=\\#]:not([href=\\#])').click(function() {
 	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -6,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 	    if (target.length) {
 	      $('html,body').animate({ scrollTop: target.offset().top }, 500);
+	      if ($(this).hasClass('sethash')) {
+		      location.hash = this.hash;
+	      }
 	      return false;
 	    }
 	  }
